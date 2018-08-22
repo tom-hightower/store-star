c574edb4ae8f00ec739782830e36cf678775ded6
Tom Hightower <tom.hightower356@gmail.com>
2018-08-21 20:47:26 -0400

    Switched to search by zip code

--- server.js
+++ server.js
@@ -18,10 +18,34 @@ app.listen(3000, function () {
 
 app.post('/', function (req, res) {
     var product = req.body.product
-    var store = req.body.store
-    walmart.stores.search(store, product).then(function(data) {
-	var location = data.results[0].location.detailed[0]
-	var locationText = `The product ${product} is in section ${location.section} of aisle ${location.aisle} of zone ${location.zone} in store ${store}!`
-	res.render('index', {location: locationText, error: null})
-    });
+    var zipcode = req.body.zipcode
+
+    findStore(zipcode).then(function whenOk(store) {
+	
+	walmart.stores.search(store.no, product).then(function(data) {
+	    var location = data.results[0].location.detailed[0]
+	    var locationText = `The product ${product} is in section ${location.section} of aisle ${location.aisle} of zone ${location.zone} in store ${store.name}!`
+	    res.render('index', {location: locationText, error: null})
+	})	
+	return store
+	
+    }).catch(function notOk(err) {
+	console.error(err)
+	res.render('index', {location: null, error: err})
+    })
+    
 })
+
+var findStore = function(zipcode){
+    return new Promise(function(resolve, reject) {
+	var store = walmart.stores.byZip(zipcode).then(function(data) {
+	    return data[0]
+	})
+	
+	if (store !== null && store !== undefined) {
+	    resolve(store)
+	} else {
+	    reject('Problem with Zip code')
+	}
+    })
+}
